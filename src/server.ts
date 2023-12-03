ec04d67ef78e79414cd0eae9721df56f2220d26f
KhalidMimMuzahid <khalidmimm@gmail.com>
2023-12-03 15:32:34 +0600

    template updated

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ import config from './app/config';
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    const conn = await mongoose.connect(config.database_url as string);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
